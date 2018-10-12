eed7b694b9b2af08d39530f57262359856b57d3b
ux-lottes <44097429+ux-lottes@users.noreply.github.com>
2018-10-12 23:25:42 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-console.log('js initialized');
+function handleTyping () {
+ var options = {
+          strings: ["UI / UX Designer", "Frontend Developer"],
+              typeSpeed: 50,
+              starDelay: 200,
+              backDelay: 600,
+              loop: true,
+              showCursor: true,
+              cursorChar: "|"
+        }
+  var typed = new Typed(".element", options);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  handleTyping ();
+});
