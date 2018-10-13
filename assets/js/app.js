bfe18a1f60e7ff92bb09919923fd8a64aea7c435
ux-lottes <44097429+ux-lottes@users.noreply.github.com>
2018-10-13 07:43:02 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,11 @@ function handleTyping () {
 document.addEventListener("DOMContentLoaded", function(event) { 
   handleTyping ();
 });
+
+/* soft scroll */
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
